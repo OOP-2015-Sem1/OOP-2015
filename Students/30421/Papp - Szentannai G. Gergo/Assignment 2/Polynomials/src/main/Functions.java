c1c771822a9013f98d99a2fe1ff5addcbe580050
Papp Szentannai Gergő <gergo_13@yahoo.com>
2015-10-19 01:26:23 +0300

    Polynomials
    Added ADD function

--- Students/30421/Papp - Szentannai G. Gergo/Assignment 2/Polynomials/src/main/Functions.java
+++ Students/30421/Papp - Szentannai G. Gergo/Assignment 2/Polynomials/src/main/Functions.java
@@ -4,14 +4,21 @@ import main.Polynomial;
 
 public class Functions {
 
-	private int max(int a, int b) {
+	private static int max(int a, int b) {
 		if (a > b)
 			return a;
 		else
 			return b;
 	}
 
-	public Polynomial ADD(Polynomial a, Polynomial b) {
+	private static int min(int a, int b) {
+		if (a < b)
+			return a;
+		else
+			return b;
+	}
+
+	public static Polynomial ADD(Polynomial a, Polynomial b) {
 		int degC = max(a.getDegree(), b.getDegree());
 		Polynomial c = new Polynomial(degC);
 		c.initArray();
@@ -19,8 +26,17 @@ public class Functions {
 		int[] A = a.getPol();
 		int[] B = b.getPol();
 
-		for (int i = 0; i < degC; i++) {
+		for (int i = 0; i < min(a.getDegree(), b.getDegree()); i++) {
 			int temp = A[i] + B[i];
+			c.updatePol(i, temp);
+		}
+		for (int i = min(a.getDegree(), b.getDegree()); i < degC; i++) {
+			int temp;
+			if (a.getDegree() > b.getDegree()) {
+				temp = A[i];
+			} else {
+				temp = B[i];
+			}
 			c.updatePol(i, temp);
 		}
 		return c;
--- Students/30421/Papp - Szentannai G. Gergo/Assignment 2/Polynomials/src/main/Main.java
+++ Students/30421/Papp - Szentannai G. Gergo/Assignment 2/Polynomials/src/main/Main.java
@@ -2,29 +2,36 @@ package main;
 
 import main.Polynomial;
 import main.Functions;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Polynomial P1 = new Polynomial(5);
 		P1.initArray();
-		for(int i = 0; i<5; i++){
+		for (int i = 0; i < 5; i++) {
 			P1.updatePol(i, i);
 		}
-		for (int i=0;i<5;i++){
+		System.out.println("First Pol:");
+		for (int i = 0; i < 5; i++) {
 			System.out.println(P1.getPol()[i]);
 		}
-		
-		Polynomial P2 = new Polynomial(5);
+
+		Polynomial P2 = new Polynomial(6);
 		P2.initArray();
-		for(int i = 0; i<5; i++){
+		for (int i = 0; i < 6; i++) {
 			P2.updatePol(i, 2);
 		}
-		for (int i=0;i<5;i++){
+		System.out.println("Second Pol:");
+		for (int i = 0; i < 6; i++) {
 			System.out.println(P2.getPol()[i]);
 		}
+		System.out.println("ADD:");
+		Polynomial P3 = Functions.ADD(P1, P2);
+
+		for (int i = 0; i < 6; i++) {
+			System.out.println(P3.getPol()[i]);
+		}
 	}
-
-
 
 }
