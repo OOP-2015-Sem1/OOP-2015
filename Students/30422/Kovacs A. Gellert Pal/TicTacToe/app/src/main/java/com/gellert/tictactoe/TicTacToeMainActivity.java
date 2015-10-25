0a84ec0173dc7d6e563fbdf4339c758ec0e602f9
Kovács Gellért Pál <kovacsgellertpal@gmail.com>
2015-10-25 14:19:05 +0200

    Final update to TicTacToe

--- Students/30422/Kovacs A. Gellert Pal/TicTacToe/app/src/main/java/com/gellert/tictactoe/TicTacToeMainActivity.java
+++ Students/30422/Kovacs A. Gellert Pal/TicTacToe/app/src/main/java/com/gellert/tictactoe/TicTacToeMainActivity.java
@@ -9,7 +9,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,12 +21,9 @@ public class TicTacToeMainActivity extends AppCompatActivity {
     private static final int O_WINS = 2;
     private static final int NO_WINNER = 0;
 
-    private int scoreX;
-    private int scoreO;
-
     private TextView scoreText;
 
-    private int counter;
+    private GameState gameStateInstance;
 
     private TicTacToeButton[][] buttonMatrix;
 
@@ -36,19 +32,18 @@ public class TicTacToeMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gameStateInstance = new GameState(); //state and scores are initialized with value 0
         loadScore();
         scoreText = (TextView) findViewById(R.id.scoreText);
-        scoreText.setText(scoreX + " - " + scoreO);
-
-        counter = 0;
+        gameStateInstance.displayScore(scoreText);
 
         buttonMatrix = new TicTacToeButton[3][3];
         buttonMatrix[0][0] = (TicTacToeButton) findViewById(R.id.btn00);
         buttonMatrix[0][1] = (TicTacToeButton) findViewById(R.id.btn01);
         buttonMatrix[0][2] = (TicTacToeButton) findViewById(R.id.btn02);
         buttonMatrix[1][0] = (TicTacToeButton) findViewById(R.id.btn10);
-        buttonMatrix[1][0] = (TicTacToeButton) findViewById(R.id.btn11);
-        buttonMatrix[1][0] = (TicTacToeButton) findViewById(R.id.btn12);
+        buttonMatrix[1][1] = (TicTacToeButton) findViewById(R.id.btn11);
+        buttonMatrix[1][2] = (TicTacToeButton) findViewById(R.id.btn12);
         buttonMatrix[2][0] = (TicTacToeButton) findViewById(R.id.btn20);
         buttonMatrix[2][1] = (TicTacToeButton) findViewById(R.id.btn21);
         buttonMatrix[2][2] = (TicTacToeButton) findViewById(R.id.btn22);
@@ -59,33 +54,30 @@ public class TicTacToeMainActivity extends AppCompatActivity {
                 buttonMatrix[i][j].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View btn) {
-                        if (counter % 2 == 0) {
-                            ((TicTacToeButton)btn).updateContent(CHAR_X);
-                        } else {
-                            ((TicTacToeButton)btn).updateContent(CHAR_O);
-                        }
-                        counter++;
+
+                        ((TicTacToeButton)btn).updateContent(gameStateInstance.currentCharacter());
+                        gameStateInstance.nextState();
+
                         switch (GameUtilities.checkWin(buttonMatrix)) {
                             case X_WINS:
                                 Toast.makeText(getApplicationContext(), "X wins!", Toast.LENGTH_SHORT).show();
-                                scoreX++;
-                                scoreText.setText(scoreX + " - " + scoreO);
+                                gameStateInstance.incScore(CHAR_X);
+                                gameStateInstance.displayScore(scoreText);
                                 resetMatch();
                                 break;
                             case O_WINS:
                                 Toast.makeText(getApplicationContext(), "O wins!", Toast.LENGTH_SHORT).show();
-                                scoreO++;
-                                scoreText.setText(scoreX + " - " + scoreO);
+                                gameStateInstance.incScore(CHAR_O);
+                                gameStateInstance.displayScore(scoreText);
                                 resetMatch();
                                 break;
                             case NO_WINNER:
-                                if (counter >= 9) {
+                                if (gameStateInstance.getState() >= 9) {
                                     Toast.makeText(getApplicationContext(), "Draw match!", Toast.LENGTH_SHORT).show();
                                     resetMatch();
                                 }
                                 break;
                         }
-
                         saveScore();
                     }
                 });
@@ -131,9 +123,9 @@ public class TicTacToeMainActivity extends AppCompatActivity {
         outState.putIntArray("board_row0", boardMatrix[0]);
         outState.putIntArray("board_row1", boardMatrix[1]);
         outState.putIntArray("board_row2", boardMatrix[2]);
-        outState.putInt("scoreX", scoreX);
-        outState.putInt("scoreO", scoreO);
-        outState.putInt("counter", counter);
+        outState.putInt("scoreX", gameStateInstance.getScore(CHAR_X));
+        outState.putInt("scoreO", gameStateInstance.getScore(CHAR_O));
+        outState.putInt("gameState", gameStateInstance.getState());
         super.onSaveInstanceState(outState);
     }
 
@@ -144,18 +136,16 @@ public class TicTacToeMainActivity extends AppCompatActivity {
         boardMatrix[0] = savedInstanceState.getIntArray("board_row0");
         boardMatrix[1] = savedInstanceState.getIntArray("board_row1");
         boardMatrix[2] = savedInstanceState.getIntArray("board_row2");
-        scoreX = savedInstanceState.getInt("scoreX");
-        scoreO = savedInstanceState.getInt("scoreO");
-        counter = savedInstanceState.getInt("counter");
+        gameStateInstance.setScore(CHAR_X, savedInstanceState.getInt("scoreX"));
+        gameStateInstance.setScore(CHAR_O, savedInstanceState.getInt("scoreO"));
+        gameStateInstance.setState(savedInstanceState.getInt("gameState"));
 
         for (int i =0; i< 3; i++) {
             for (int j = 0; j<3;j++) {
                 buttonMatrix[i][j].updateContent(boardMatrix[i][j]);
             }
         }
-
-        scoreText.setText(scoreX + " - " + scoreO);
-
+        gameStateInstance.displayScore(scoreText);
         super.onRestoreInstanceState(savedInstanceState);
     }
 
@@ -165,28 +155,26 @@ public class TicTacToeMainActivity extends AppCompatActivity {
                 buttonMatrix[i][j].resetContent();
             }
         }
-
-        counter = 0;
+        gameStateInstance.resetState();
     }
 
     private void resetScore() {
-        scoreO = 0;
-        scoreX = 0;
-        scoreText.setText("0 - 0");
+        gameStateInstance.resetScore();
+        gameStateInstance.displayScore(scoreText);
         saveScore();
     }
 
     public void saveScore() {
         SharedPreferences sP = this.getSharedPreferences("scoreInfo", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sP.edit();
-        editor.putInt("scoreX",scoreX);
-        editor.putInt("scoreO",scoreO);
+        editor.putInt("scoreX",gameStateInstance.getScore(CHAR_X));
+        editor.putInt("scoreO",gameStateInstance.getScore(CHAR_O));
         editor.apply();
     }
 
     public void loadScore() {
         SharedPreferences sP = this.getSharedPreferences("scoreInfo", Context.MODE_PRIVATE);
-        scoreX = sP.getInt("scoreX", 0);
-        scoreO = sP.getInt("scoreO", 0);
+        gameStateInstance.setScore(CHAR_X, sP.getInt("scoreX", 0));
+        gameStateInstance.setScore(CHAR_O, sP.getInt("scoreO", 0));
     }
 }
