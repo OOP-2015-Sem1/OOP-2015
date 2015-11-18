d4dc7349545351e30c120572a8ba77eee1653b20
teofilZaharia <teofil2010t@gmail.com>
2015-11-18 14:26:23 +0200

    Fluffy Swing Graphics

---
+++ Students/30421/Zaharia M. Teofil/Fluffy the Cat/src/fluffly/the/cat/game/Main.java
@@ -0,0 +1,11 @@
+package fluffly.the.cat.game;
+
+import fluffy.the.cat.io.GraphicBoardPrinter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Game(new GraphicBoardPrinter());
+	}
+
+}
