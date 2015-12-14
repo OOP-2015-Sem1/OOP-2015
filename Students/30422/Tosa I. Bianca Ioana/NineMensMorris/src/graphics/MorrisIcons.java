59b5d1013804827514592aa597b56ebe87a853bc
Tosa Bianca <bianca.tosa@yahoo.com>
2015-12-14 20:58:52 +0200

    Nine Men's Morris - modified

---
+++ Students/30422/Tosa I. Bianca Ioana/NineMensMorris/src/graphics/MorrisIcons.java
@@ -0,0 +1,24 @@
+package graphics;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class MorrisIcons {
+
+	private static MorrisIcons instance;
+
+	public static MorrisIcons getInstance() {
+		if (instance == null) {
+			instance = new MorrisIcons();
+		}
+		return instance;
+	}
+
+	private MorrisIcons() {
+	}
+	public Icon VL = new ImageIcon(getClass().getResource("V_Line.png"));
+	public Icon OL = new ImageIcon(getClass().getResource("O_Line.png"));
+	public Icon Button = new ImageIcon(getClass().getResource("Button.png"));
+	public Icon R = new ImageIcon(getClass().getResource("Red.png"));
+	public Icon B = new ImageIcon(getClass().getResource("Black.png"));
+	
+}
---
+++ Students/30422/Tosa I. Bianca Ioana/NineMensMorris/src/logic/Main.java
@@ -0,0 +1,12 @@
+package logic;
+import graphics.GUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new GUI();
+		
+
+	}
+
+}
