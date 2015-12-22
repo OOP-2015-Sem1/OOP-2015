efe2219971705ec4ab778e7f574336081eb41ec3
ranapop <roxy_rox1995@yahoo.com>
2015-12-23 01:44:12 +0200

    This is my chess project so far!

---
+++ Students/Others/Pop P. Roxana/Chess/app/src/main/java/com/example/roxanappop/chess/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.roxanappop.chess;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.roxanappop.chess.Controller.Controller;
+
+public class MainActivity extends AppCompatActivity implements PlayButtonFragment.FragementActivity_I{
+
+    Controller controller;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // add fragment play to the activity
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager
+                .beginTransaction();
+        PlayButtonFragment playButtonFragment = new PlayButtonFragment();
+        fragmentTransaction.replace(R.id.fragment, playButtonFragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Controller getControllerInstance() {
+
+        controller = new Controller(this);
+        return controller;
+    }
+}
