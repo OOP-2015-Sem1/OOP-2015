253488fcfe65078bec879800750a2bb633e42f58
Filip Sebastian <filip.seba@gmail.com>
2015-12-22 14:10:07 +0200

    I deleted some things that were not so important. I still don't get it why the buttons with the name of students are not extended to all the width of the Panel.

--- Students/30421/Filip O. Sebastian Mihai/Digital school grading book/src/catalog/ui/ClassroomsFromSchoolPanel.java
+++ Students/30421/Filip O. Sebastian Mihai/Digital school grading book/src/catalog/ui/ClassroomsFromSchoolPanel.java
@@ -1,6 +1,5 @@
 package catalog.ui;
 
-import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,15 +7,13 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-import catalog.brain.Main;
+import catalog.brain.Students;
 
 public class ClassroomsFromSchoolPanel extends JPanel implements ActionListener {
 	private static final long serialVersionUID = 0000;
-	Container c;
 	public static int WhatGradeToShow = 12;
 	private StudentsFromEachGradePanel PanelForGrades = new StudentsFromEachGradePanel();
 	private JButton[] ClassroomsButtons;
-	public JPanel[] PanelForGrade = new JPanel[13];
 	public JPanel StudentsFromClassrooms = new JPanel();
 
 	public ClassroomsFromSchoolPanel() {
@@ -24,21 +21,14 @@ public class ClassroomsFromSchoolPanel extends JPanel implements ActionListener 
 		setLayout(new GridLayout(12, 1));
 		ClassroomsButtons = new JButton[12];
 		setVisible(false);
-		// System.out.println(fau.Classrooms.size());
 		for (int i = 0; i < 12; i++) {
-			ClassroomsButtons[i] = new JButton(Main.stud.Classrooms.get(i));
+			ClassroomsButtons[i] = new JButton(Students.Classrooms.get(i));
 			add(ClassroomsButtons[i]);
 			ClassroomsButtons[i].addActionListener(this);
 		}
-		// StudentsFromClassrooms.setLayout(new GridLayout(1,1));
-		for (int i = 0; i < 13; i++) {
-			PanelForGrade[i] = new JPanel();
-			PanelForGrade[i].setVisible(false);
-			PanelForGrade[i] = PanelForGrades.StudentsPanelFromClasroom[i];
-		}
-
+		//StudentsFromClassrooms.setLayout();
 		for (int i = 1; i < 13; i++)
-			StudentsFromClassrooms.add(PanelForGrade[i]);
+			StudentsFromClassrooms.add(PanelForGrades.StudentsPanelFromClasroom[i]);
 		StudentsFromClassrooms.setVisible(true);
 	}
 
@@ -48,10 +38,10 @@ public class ClassroomsFromSchoolPanel extends JPanel implements ActionListener 
 		for (int j = 0; j < 12; j++) {
 			if (e.getSource() == ClassroomsButtons[j]) {
 				for (int i = 1; i < 13; i++) {
-					if (i == j+1)
-						PanelForGrade[i].setVisible(true);
+					if (i == j + 1)
+						PanelForGrades.StudentsPanelFromClasroom[i].setVisible(true);
 					else
-						PanelForGrade[i].setVisible(false);
+						PanelForGrades.StudentsPanelFromClasroom[i].setVisible(false);
 				}
 			}
 		}
