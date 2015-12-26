0aa7fb6422d5b1bd33ed5116e7f9fb98ca9d7e5b
CalugarCatalina <calugar_catalina_andreea@yahoo.com>
2015-12-26 20:13:10 +0200

    Some modifications...

--- Students/30422/Calugar F. Catalina Andreea/Snake1/src/entities/Constants.java
+++ Students/30422/Calugar F. Catalina Andreea/Snake1/src/entities/Constants.java
@@ -1,4 +1,4 @@
-package gui;
+package entities;
 
 import java.awt.Color;
 
@@ -12,9 +12,13 @@ public class Constants {
 	public static final String RIGHT = "RIGHT";
 	public static final String STEP = "STEP";
 
+	public static final int MIN_BOARD_SIZE = 10;
+	public static final int MAX_BOARD_SIZE = 20;
+	public static final int DEFAULT_BOARD_SIZE = 20;
+
 	public static final LineBorder LINE_BORDER = new LineBorder(Color.GRAY);
 	public static boolean canWeGoThroughWalls;
+	public static int BOARD_SIZE = 20;
+	public static final int DIMENSION = 800;
 
-	public static final int WIDTH = 800;
-	public static final int HEIGHT = 800;
 }
