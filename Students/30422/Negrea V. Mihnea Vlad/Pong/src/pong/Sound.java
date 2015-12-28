6b2e919bef3ec7834789e44bfd6f4f0a0350b4c5
Negrea Vlad <negrea.vlad@yahoo.com>
2015-12-28 21:56:56 +0200

    asd

---
+++ Students/30422/Negrea V. Mihnea Vlad/Pong/src/pong/Sound.java
@@ -0,0 +1,25 @@
+package pong;
+
+
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+import java.io.*;
+
+public class Sound {
+
+	public static void PlaySound(File Sound) {
+
+		try {
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(Sound));
+			clip.start();
+
+		} catch (Exception error) {
+
+		}
+		
+	
+	}
+}
