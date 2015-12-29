dd2a18c4c031ab03440716c687e7685f78ffc802
iacobvlad <vlad.iacob.ionut@outlook.com>
2015-12-29 22:29:29 +0200

    This is the actual project !!

---
+++ Students/30422/Iacob I. Vlad Ionut/Breakout1/src/NewGame.java
@@ -0,0 +1,12 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class NewGame implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
