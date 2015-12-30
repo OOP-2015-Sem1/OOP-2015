13bdf8203c67ba709527fa11944cd902a6b312aa
nrares <nistor_rares@yahoo.com>
2015-12-30 11:51:50 +0200

    A more final form of the Game

--- Students/30425/Nistor A. Rares Constantin/asd/src/asd/Deck.java
+++ Students/30425/Nistor A. Rares Constantin/asd/src/asd/Deck.java
@@ -4,36 +4,32 @@ import java.util.Random;
 
 public class Deck {
 
+	private Card[] cards;
+	private int nrOfCards = 0;
 
-    private Card[] cards;
-    private int nrOfCards = 0;
+	public Deck() {
+		cards = new Card[52];
+			for (int i = 0; i < 4; i++)
+				for (int j = 0; j < 13; j++) {
+					cards[nrOfCards] = new Card(j, "/cards\\" + i + "" + j + ".png");
+					nrOfCards++;
+				}
+	
+	}
 
-    public Deck() {
-        cards = new Card[52];
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 13; j++) {
-                cards[nrOfCards] = new Card(j, i +""+ j + ".png");// the string is used so I can import pictures
-                nrOfCards++;                                      // more easely
-            }
-        }
-    }
+	public Card dealCard() {
+		return cards[--nrOfCards];
+	}
 
-    public Card dealCard() {
-        return cards[--nrOfCards];
-    }
-
- 
-
-    public void shuffle() {
-       Random r = new Random();
-       Card swap;
-       int j;
-       for(int i=0; i<nrOfCards; i++){
-    	   j = r.nextInt(nrOfCards);
-    	   swap =cards[i];
-    	   cards[i]=cards[j];
-    	   cards[j]=swap;
-       }
-    }
+	public void shuffle() {
+		Random r = new Random();
+		Card swap;
+		int j;
+		for (int i = 0; i < nrOfCards; i++) {
+			j = r.nextInt(nrOfCards);
+			swap = cards[i];
+			cards[i] = cards[j];
+			cards[j] = swap;
+		}
+	}
 }
-
