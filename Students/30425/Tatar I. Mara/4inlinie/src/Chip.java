c2ed492f75a74b3eadeb92e48ef3cb4ac11f65fe
Mara <maratatar@yahoo.com>
2016-01-02 16:06:54 +0200

    4 connect.

---
+++ Students/30425/Tatar I. Mara/4inlinie/src/Chip.java
@@ -0,0 +1,8 @@
+
+public enum Chip {
+	
+	BLANK, 
+	YELLOW,
+	RED;
+
+}
---
+++ Students/30425/Tatar I. Mara/4inlinie/src/Connect4.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Connect4 {
+
+	public static void main(String[] args) {
+		Connect4Frame frame = new Connect4Frame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		// frame.setSize(600,700);
+	}
+}
