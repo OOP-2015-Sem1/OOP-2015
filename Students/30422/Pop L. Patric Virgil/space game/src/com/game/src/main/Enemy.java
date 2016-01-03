7a51b31a5b0c69d08f6fbe71c1d07e69e2f41e27
Pop Patric Virgil <pop_patric@yahoo.com>
2016-01-03 13:47:27 +0200

    This is my project!

---
+++ Students/30422/Pop L. Patric Virgil/space game/src/com/game/src/main/Enemy.java
@@ -0,0 +1,69 @@
+package com.game.src.main;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.Random;
+
+import com.game.src.main.classes.EntityA;
+import com.game.src.main.classes.EntityB;
+
+public class Enemy extends GameObject implements EntityB{
+	
+	
+	
+	private Textures tex ;
+	Random r=new Random();
+	private Game game;
+	private Controller c;
+	private int speed=r.nextInt(3)+1;
+	
+	public Enemy(double x,double y,Textures tex,Controller c,Game game){
+		super(x,y);
+		this.tex=tex;
+		this.c=c;
+		this.game=game;
+	}
+	
+	public void tick(){
+		y+=speed;
+		if(y>Game.HEIGHT*Game.SCALE){
+			y=-10;
+			x=r.nextInt(640);
+		}
+		
+		for(int i=0;i<game.ea.size();i++){
+			EntityA tempEnt=game.ea.get(i);
+			if(Physics.Collision(this, tempEnt))
+			{
+				c.removeEntity(tempEnt);
+				c.removeEntity(this);
+				game.setEnemy_killed(game.getEnemy_killed()+1);
+			}
+		}
+		
+		
+	
+		
+	}
+	
+	public void render(Graphics g){
+		g.drawImage(tex.enemy, (int)x, (int)y, null);
+		
+	}
+	public Rectangle getBounds(){
+		return new Rectangle((int)x,(int)y,32,32);
+	}
+
+	@Override
+	public double getX() {
+
+		return x;
+	}
+
+	
+	public double getY() {
+		
+		return y;
+	}
+
+}
