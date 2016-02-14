5f3b286c5c20341644f19443d8fc6b775b2e45bd
DariLupea <dari.lupea@gmail.com>
2016-02-14 19:13:42 +0200

    Final project

--- Students/30425/Lupea V. Dariana/PuzzleG/src/Controller/Main.java
+++ Students/30425/Lupea V. Dariana/PuzzleG/src/Controller/Main.java
@@ -1,33 +1,11 @@
 package Controller;
 
-import java.util.Scanner;
 import View.Puzzle;
 
 public class Main {
 
-	private Scanner name;
-	private String userName;
-
-	public void userName() {
-		name = new Scanner(System.in);
-		System.out.println("Give your name and press Enter:");
-		setUserName(name.nextLine());
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
 	public static void main(String args[]) {
-
-		Main m = new Main();
-		m.userName();
 		new Puzzle("Images/bear");
-
 	}
 
 }
